d3b89df3fd9d59fc98fb4e97ad65e1aa2b574799
Aman Sharma <amanhsharma7@gmail.com>
2026-02-01 15:53:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from textblob import TextBlob 
+from newspaper import Article
+
+url = ''
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+text = article.summary
+print(text)
+
+blob = TextBlob(text)
+
+sentiment = blob.sentiment.polarity
+print(sentiment)
